6b140b62a847cd9f729460c847aa1f1ee605ab39
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-06-22 15:08:12 +0200

    Refactor apps code using jquery

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,5 @@
 import Timer from './timer.js';
 
-const tempoDisplay = document.querySelector('.tempo-value');
-const tempoDescription = document.querySelector('.tempo-description');
-const tempoSlider = document.querySelector('.tempo-slider');
-const decreaseBtn = document.querySelector('.decrease-tempo-btn');
-const increaseBtn = document.querySelector('.increase-tempo-btn');
-const subtractBeats = document.querySelector('.subtract-beats-btn');
-const addBeats = document.querySelector('.add-beats-btn');
-const subtractNotes = document.querySelector('.subtract-notes-btn');
-const addNotes = document.querySelector('.add-notes-btn');
-const beatsValue = document.querySelector('.display-beats');
-const notesValue = document.querySelector('.display-notes');
-const playBtn = document.querySelector('.play-btn');
-
 const click1 = new Audio('/media/click1.mp3');
 const click2 = new Audio('/media/click2.mp3');
 
@@ -38,100 +25,99 @@ let count = 0;
 let isRunning = false;
 
 // change tempo value
-decreaseBtn.addEventListener('click', () => {
+$('.decrease-tempo-btn').on('click', () => {
     bpm--;
-    validateTempo()
-    updateMetronome()
-});
-increaseBtn.addEventListener('click', () => {
-    bpm++;
-    validateTempo()
-    updateMetronome()
+    validateTempo();
 });
 
-tempoSlider.addEventListener('input', () => {
-    bpm = tempoSlider.value;
-    validateTempo()
-    updateMetronome()
+$('.increase-tempo-btn').on('click', () => {
+    bpm++;
+    validateTempo();
+});
+
+$('.tempo-slider').on('input', () => {
+    bpm = $('.tempo-slider').val();
+    validateTempo();
 });
 
 
 // change measure - beats
-subtractBeats.addEventListener('click', () => {
-    if (beats <= 1) { return }
+$('.subtract-beats-btn').on('click', () => {
+    if (beats <= 1) { return };
     beats--;
-    beatsValue.textContent = beats;
+    $('.display-beats').text(beats);
     count = 0;
 });
 
-addBeats.addEventListener('click', () => {
-    if (beats >= 12) { return }
+$('.add-beats-btn').on('click', () => {
+    if (beats >= 12) { return };
     beats++;
-    beatsValue.textContent = beats;
+    $('.display-beats').text(beats);
     count = 0;
 });
 
 
 // change measure - notes
-subtractNotes.addEventListener('click', () => {
-    if (notes <= 1) { return }
+$('.subtract-notes-btn').on('click', () => {
+    if (notes <= 1) { return };
     notes /= 2;
-    notesValue.textContent = notes;
-    updateMetronome()
+    $('.display-notes').text(notes);
+    updateMetronome();
     count = 0;
 });
 
-addNotes.addEventListener('click', () => {
-    if (notes >= 16) { return }
+$('.add-notes-btn').on('click', () => {
+    if (notes >= 16) { return };
     notes *= 2;
-    notesValue.textContent = notes;
-    updateMetronome()
+    $('.display-notes').text(notes);
+    updateMetronome();
     count = 0;
 });
 
 
 // play button
-playBtn.addEventListener('click', () => {
+$('.play-btn').on('click', () => {
     count = 0;
     if (!isRunning) {
         metronome.start();
         isRunning = true;
-        playBtn.classList.add('is-playing');
+        $(this).addClass('is-playing');
     } else {
         metronome.stop();
         isRunning = false;
-        playBtn.classList.remove('is-playing');
-    }
-})
+        $(this).removeClass('is-playing');
+    };
+});
 
 
 function updateMetronome() {
-    tempoDisplay.textContent = bpm;
+    $('.tempo-value').text(bpm);
     metronome.timeInterval = 240000 / bpm / notes;
     for (let i = 0; i < tempoDescriptionsArray.length; i++) {
         if (bpm > tempoDescriptionsArray[i][0] && bpm < tempoDescriptionsArray[i][1]) {
-            tempoDescriptionString = tempoDescriptionsArray[i][2]}
-        }
-    tempoDescription.textContent = tempoDescriptionString;
-}
+            tempoDescriptionString = tempoDescriptionsArray[i][2]};
+        };
+    $('.tempo-description').text(tempoDescriptionString);
+};
 
 function validateTempo() {
     if (bpm <= 20) { bpm = 20 };
     if (bpm >= 250) { bpm = 250 };
-}
+    updateMetronome();
+};
 
 function playClick() {
     if (count === beats) {
         count = 0;
-    }
+    };
     if (count === 0) {
         click1.play();
         click1.currentTime = 0;
     } else {
         click2.play();
         click2.currentTime = 0;
-    }
+    };
     count++;
-}
+};
 
 const metronome = new Timer(playClick, 240000 / bpm / notes, { immediate: true });
